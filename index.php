ab3c703434dc598ba67d7e98085f74d19bd070e6
PIUnivespgrupo006 <23208844@aluno.univesp.br>
2025-04-07 10:47:41 -0300

    Create index.php
    Cria o indice com o menu de opções inicial.

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Controle de Estoque</title>
+    <link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Controle de Estoque</h1>
+        <p>Selecione uma opção abaixo:</p>
+
+        <div class="menu">
+            <a href="cadastrar_produto.php"><button>Cadastrar Produto</button></a>
+            <a href="registrar_venda.php"><button>Registrar Venda</button></a>
+            <a href="adicionar_estoque.php"><button>Adicionar Estoque</button></a>
+            <a href="relatorio_reposicao.php"><button>Relatório de Reposição</button></a>
+        </div>
+    </div>
+</body>
+</html>
